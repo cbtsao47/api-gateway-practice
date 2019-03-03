b9a0d820813e28fe12d09cd55a198e9fec407d0a
wisdomtooth <cbtsao47@gmail.com>
2019-03-03 15:44:24 -0600

    set up server

---
+++ index.js
@@ -0,0 +1,18 @@
+//import express
+const express = require("express");
+// starts a server with express();
+const app = express();
+const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Simple API Gateway");
+});
+
+app.use(router);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`API Gateway running on localhost ${port}`);
+});
